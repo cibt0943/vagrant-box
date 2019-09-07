54b5de409d274b6d96c81a18db6c7fe95a878e24
cibt0943 <>
2019-09-07 15:33:17 +0900

    vbguestをインストールしない

--- box_build/vagrant/centos7_ruby2.6/scripts/provision.sh
+++ box_build/vagrant/centos7_ruby2.6/scripts/provision.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # gitインストール
-yum -y install git
+sudo yum -y install git
 
 # rbenvのインストール
-git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
-
-# ruby-buildのインストール
-git clone git://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
 # PATH設定
-echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/rbenv.sh
-echo 'export PATH=$RBENV_ROOT/bin:$PATH' >> /etc/profile.d/rbenv.sh
-echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
-source /etc/profile.d/rbenv.sh
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+# ruby-buildのインストール
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 # rubyのビルドに必要なライブラリのインストール
-yum -y install bzip2 gcc openssl-devel readline-devel zlib-devel
+sudo yum -y install bzip2 gcc openssl-devel readline-devel zlib-devel
 
 # rubyインストール
 rbenv install -v $1
 
 # 利用するバージョンの設定
 rbenv global $1
+rbenv rehash
 
 # bundlerのインストール
-rbenv exec gem install bundler -N
+# rbenv exec gem install bundler -N
 
 
 # 確認用
